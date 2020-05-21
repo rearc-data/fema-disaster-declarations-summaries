3e89e34913b265607d62bdb172baca379b841071
Derek Wolpert <me@derekwolpert.com>
2020-05-21 16:04:32 -0400

    add retry logic to downloading files

--- pre-processing/pre-processing-code/source_data.py
+++ pre-processing/pre-processing-code/source_data.py
@@ -1,4 +1,5 @@
 import os
+import time
 import boto3
 from urllib.request import urlopen
 from urllib.error import URLError, HTTPError
@@ -11,37 +12,42 @@ def data_to_s3(endpoint):
 
 	source_dataset_url = 'https://www.fema.gov/api/open/v2/DisasterDeclarationsSummaries'
 
-	try:
-		response = urlopen(source_dataset_url + endpoint)
+	retries = 5
+	for attempt in range(retries):
+		try:
+			response = urlopen(source_dataset_url + endpoint)
+			break
+		except HTTPError as e:
+			if attempt == retries:
+				raise Exception('HTTPError: ', e.code, endpoint)
+			time.sleep(0.2 * attempt)
 
-	except HTTPError as e:
-		raise Exception('HTTPError: ', e.code, endpoint)
+		except URLError as e:
+			if attempt == retries:
+				raise Exception('URLError: ', e.reason, endpoint)
+			time.sleep(0.2 * attempt)
 
-	except URLError as e:
-		raise Exception('URLError: ', e.reason, endpoint)
+	data_set_name = os.environ['DATA_SET_NAME']
+	filename = data_set_name + endpoint
+	file_location = '/tmp/' + data_set_name + endpoint
 
-	else:
-		data_set_name = os.environ['DATA_SET_NAME']
-		filename = data_set_name + endpoint
-		file_location = '/tmp/' + data_set_name + endpoint
+	with open(file_location, 'wb') as f:
+		f.write(response.read())
 
-		with open(file_location, 'wb') as f:
-			f.write(response.read())
+	# variables/resources used to upload to s3
+	s3_bucket = os.environ['S3_BUCKET']
+	new_s3_key = data_set_name + '/dataset/' + filename
+	s3 = boto3.client('s3')
 
-		# variables/resources used to upload to s3
-		s3_bucket = os.environ['S3_BUCKET']
-		new_s3_key = data_set_name + '/dataset/' + filename
-		s3 = boto3.client('s3')
+	s3.upload_file(file_location, s3_bucket, new_s3_key)
 
-		s3.upload_file(file_location, s3_bucket, new_s3_key)
+	print('Uploaded: ' + filename)
 
-		print('Uploaded: ' + filename)
+	# deletes to preserve limited space in aws lamdba
+	os.remove(file_location)
 
-		# deletes to preserve limited space in aws lamdba
-		os.remove(file_location)
-
-		# dicts to be used to add assets to the dataset revision
-		return {'Bucket': s3_bucket, 'Key': new_s3_key}
+	# dicts to be used to add assets to the dataset revision
+	return {'Bucket': s3_bucket, 'Key': new_s3_key}
 
 def source_dataset():
 
